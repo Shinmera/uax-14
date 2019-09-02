c3aae6cfb141fe44b95ad1e71c9fb0daf9734d66
Shinmera <shinmera@tymoon.eu>
2019-09-02 22:59:12 +0200

    Preliminary pair table compilation (table not yet updated fully, copied from UAX14-R37)

--- compile.lisp
+++ compile.lisp
@@ -12,8 +12,6 @@
 
 (in-package #:org.shirakumo.alloy.uax-14.compiler)
 
-(defvar *default-source* (make-pathname :name "LineBreak" :type "txt" :defaults uax-14::*here*))
-
 (defvar *defaults*
   '((#x20A0 #x20CF :PR)
     (#x3400 #x4DBF :ID)
@@ -40,7 +38,7 @@
     (loop for i from start to end
           do (setf (aref data i) value))))
 
-(defun compile-line-break-database (&key (source *default-source*)
+(defun compile-line-break-database (&key (source (make-pathname :name "LineBreak" :type "txt" :defaults uax-14::*here*))
                                          (target uax-14:*line-break-database-file*))
   (let ((data (make-array #x10FFFF :element-type '(unsigned-byte 8)
                                    :initial-element (uax-14::type-id :XX))))
@@ -59,3 +57,40 @@
                             :element-type '(unsigned-byte 8))
       (write-sequence data stream))
     target))
+
+(defun split (split string)
+  (let ((parts ()) (buffer (make-string-output-stream)))
+    (flet ((maybe-output ()
+             (let ((part (get-output-stream-string buffer)))
+               (when (string/= part "") (push part parts)))))
+      (loop for char across string
+            do (if (char= char split)
+                   (maybe-output)
+                   (write-char char buffer))
+            finally (maybe-output))
+      (nreverse parts))))
+
+(defun compile-pair-table (&key (source (make-pathname :name "PairTable" :type "txt" :defaults uax-14::*here*))
+                                (target uax-14:*pair-table-file*))
+  (let ((data (make-array (expt (length uax-14::+line-break-type-map+) 2)
+                          :element-type '(unsigned-byte 8)
+                          :initial-element 0)))
+    (with-open-file (stream source
+                            :direction :input
+                            :element-type 'character
+                            :external-format :utf-8)
+      (let ((As (cdr (split #\Tab (read-line stream)))))
+        (loop for line = (read-line stream NIL)
+              while line
+              do (destructuring-bind (B . Vs) (split #\Tab line)
+                   (let ((row (uax-14::type-id (find-symbol B "KEYWORD"))))
+                     (loop for A in As
+                           for V in Vs
+                           for col = (uax-14::type-id (find-symbol A "KEYWORD"))
+                           for idx = (+ col (* row (length uax-14::+line-break-type-map+)))
+                           do (setf (aref data idx) (uax-14::pair-id (character V)))))))
+        (with-open-file (stream target
+                                :direction :output
+                                :element-type '(unsigned-byte 8))
+          (write-sequence data stream))
+        target))))
--- uax-14.lisp
+++ uax-14.lisp
@@ -7,7 +7,8 @@
 (defpackage #:org.shirakumo.alloy.uax-14
   (:use #:cl)
   (:export
-   #:*line-break-database-file*))
+   #:*line-break-database-file*
+   #:*pair-table-file*))
 
 (in-package #:org.shirakumo.alloy.uax-14)
 
@@ -16,6 +17,7 @@
                                     (error "COMPILE-FILE or LOAD this file."))))
 
 (defvar *line-break-database-file* (make-pathname :name "LineBreak" :type "dat" :defaults *here*))
+(defvar *pair-table-file* (make-pathname :name "PairTable" :type "dat" :defaults *here*))
 
 (defmacro defglobal (name value)
   #+sbcl `(sb-ext:defglobal ,name ,value)
@@ -31,7 +33,14 @@
 (defun type-id (type)
   (position type +line-break-type-map+))
 
-(defglobal +line-break-map+ (make-array #x10FFFF :element-type '(unsigned-byte 8)))
+(defglobal +pair-type-map+
+    #(#\_ #\% #\# #\@ #\^))
+
+(declaim (inline pair-id))
+(defun pair-id (pair)
+  (position pair +pair-type-map+))
+
+(defglobal +line-break-map+ (make-array #x10FFFF :element-type '(unsigned-byte 8) :initial-element 0))
 
 (defun load-line-break-database (&optional (source *line-break-database-file*))
   (with-open-file (stream source
@@ -42,15 +51,42 @@
           do (setf i (read-sequence +line-break-map+ stream :start i)))
     +line-break-map+))
 
-(when (probe-file *line-break-database-file*)
-  (load-line-break-database))
-
-(declare (inline char-line-break-id))
+(declaim (inline char-line-break-id))
 (defun char-line-break-id (char)
   (aref +line-break-map+ (char-code char)))
 
 (defun char-line-break-type (char)
   (aref +line-break-type-map+ (char-line-break-id char)))
 
+(defglobal +pair-table+ (make-array (expt (length +line-break-type-map+) 2) :element-type '(unsigned-byte 8) :initial-element 0))
+
+(defun load-pair-table (&optional (source *pair-table-file*))
+  (with-open-file (stream source
+                          :direction :input
+                          :element-type '(unsigned-byte 8))
+    (loop with i = 0
+          while (< i (length +pair-table+))
+          do (setf i (read-sequence +pair-table+ stream :start i)))
+    +pair-table+))
+
+(declaim (inline pair-type-id))
+(defun pair-type-id (b a)
+  (aref +pair-table+ (+ (char-line-break-id a)
+                        (* (char-line-break-id b)
+                           (length +line-break-type-map+)))))
+
+(defun pair-type (b a)
+  (aref +pair-type-map+ (pair-type-id b a)))
+
 (defun find-line-breaks (string breaks)
   )
+
+
+
+;;; Load the tables.
+(cond ((and (probe-file *line-break-database-file*)
+            (probe-file *pair-table-file*))
+       (load-line-break-database)
+       (load-pair-table))
+      (T
+       (format T "~&UAX-14: Database files are not available. Please run the UAX-14 compiler.")))
